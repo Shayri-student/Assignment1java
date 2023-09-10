2b91f718cb9592458bb01d70d8fb95cb4cbd5df1
Shayri-student <shariq.shahid1999@gmail.com>
2023-09-10 02:02:21 -0700

    Assignment uploaded

---
+++ app.js
@@ -0,0 +1,88 @@
+    // Chapter no 1
+     // ques 1
+     alert('First Name: Muhammad');
+     alert('Last Name: Shariq');
+     alert('Email Adress: Shariq.shahid1999@gmail.com');
+     alert('Phone Number: 0321-2312512');
+     alert('Passward: Sha2312512');
+         //   Ques 2 correct statement is
+     alert("You're learning JavaScript!");
+         // Ques 3
+     alert('I have learnt Few things in Java');
+     //  chapter no 1 ends here 
+     
+     // chapter no 2 Starts 
+     var tagName;
+     teamName="Tag your Best Friend";
+     
+     var teamName;
+     teamName="My Team name is HighTech";
+     alert(teamName);
+     var bestMan = "Charlie";
+     bestMan="Chaplan";
+     alert(bestMan);
+     // Chapter 2 Ends here 
+     
+     // Chapter 3 Starts 
+     var caseQty;
+     caseQty = 144;
+     //   Ques 3
+     var num = 9;
+     //    Ques 4
+     var num1 = 10;
+     var sum = num + num1;
+     alert(sum);
+         //  Ques 5 
+     var merchTotal = 100;
+     var shippingCharge = 10;
+     var orderTotal = merchTotal + shippingCharge;
+     alert(orderTotal);
+     //  Ques 6
+     var numb = 9;
+     numb= numb + 1;
+     alert(numb);
+     //    ques 3 Ends here
+     //  Ques 1
+     var productCost = 3.45;
+     //  Ques 2
+     var nameofBand;
+     //  Ques 3 
+     var netWeight = 140;
+     //   Ques 4 
+     var muhammadShariq;
+     //  Ques 5
+     // Legal names
+     
+     // grossWeight;
+     // teamGroup;
+     // watchTime;
+     // Illegal Names 
+     
+     // 5Number;
+     // num*;
+     // big@;
+     // group num;
+     //   Chapter 4 Ends here 
+     
+     // chapter 5 starts 
+     alert('% operator is use for finding remainder \n / operator is use for divide');
+     //    Ques 2
+     var num2 = 20 % 6;
+     alert(num2);
+     alert('2 is the value of num');
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+          
+     
