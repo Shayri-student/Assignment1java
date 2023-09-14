d8188f5006d60d30dda9c49c25e37ffeab24b2c6
Shayri-student <shariq.shahid1999@gmail.com>
2023-09-14 12:41:14 -0700

    14 to 16

---
+++ 14 to 16/app.js
@@ -0,0 +1,66 @@
+// *************************Q1*************************
+// var arr = [];
+// *************************Q2*************************
+// var arr = [1,2,3,4,5];
+// console.log(arr);
+// *************************Q3*************************
+// var arr = ["a","b","c"];
+// console.log(arr);
+// *************************Q4*************************
+// var arr = [1,2,3,4,5];
+// console.log(arr);
+// *************************Q5*************************
+// var arr = [true,false];
+// console.log(arr);
+// *************************Q6*************************
+// var arr = [1,true,"Family",5];
+// console.log(arr);
+// *************************Q7*************************
+// var arr = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M.Phil", "PhD"];
+// document.write("(1)"+arr[0]+"<br/> (2)"+ arr[1]+"<br/> (3)"+ arr[2]+"<br/> (4)"+ arr[3]+"<br/> (5)"+ arr[4]+"<br/> (6)"+ arr[5]+"<br/> (7)"+ arr[6]+"<br/> (8)"+ arr[7]);
+// *************************Q8*************************
+// var arr = ['Micheal','John','Tony'];
+// var arr1 = [320,230,480];
+// var arr2 = ['64%','46%','96%'];
+// document.write('Score of '+ arr[0]+' is '+arr1[0]+'. Percentage: '+arr2[0]+'<br/>'+'Score of '+ arr[1]+' is '+arr1[1]+'. Percentage: '+arr2[1]+'<br/>'+'Score of '+ arr[2]+' is '+arr1[2]+'. Percentage: '+arr2[2])
+
+// *************************Q9*************************
+// var clr = ['red','blue','yellow','Green'];
+// document.write(clr);
+
+// var begClr = prompt('what color you wants to add to the beginning?');
+// clr.unshift(begClr);
+// document.write('\nUpdated begining;' +clr);
+
+// var endClr = prompt('what color you wants to add to the end?');
+// clr.push(endClr);
+// document.write('\nUpdated end;' +clr);
+
+// clr.unshift('pink','magenta');
+// document.write('\nUpdated begining 2:' +clr);
+
+// clr.pop();
+// document.write('\nUpdated again:' +clr);
+
+// clr.shift();
+// document.write('\nUpdated again:' +clr);
+
+// var positionClr = prompt('At which Position you wants to add color?');
+// clr.splice(positionClr,0,'redish');
+// document.write('\nUpdated again:' +clr);
+
+// var deleteClrp = prompt('At which Position you wants to add color?');
+// clr.splice(deleteClrp,deleteClrp,'redish');
+// document.write('\nUpdated again:' +clr);
+
+
+// *************************Q10*************************
+// var arr = [320,230,480,120];
+// console.log('Score Unordered '+ arr +"<br/>" +"Ordered Score"+ arr[3]+ arr[1]+ arr[0]+ arr[2]);
+// *************************Q11*************************
+
+
+// *************************Q12*************************
+// *************************Q13*************************
+// *************************Q14*************************
+// *************************Q15*************************
