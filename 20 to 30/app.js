031ffd456a193dd3aea03d727bdf2d6c4f3a816a
Shayri-student <shariq.shahid1999@gmail.com>
2023-09-24 13:17:35 -0700

    20 to30

---
+++ 20 to 30/app.js
@@ -0,0 +1,96 @@
+// ******************Chapter#21************
+// ****************Q1**********************
+// var allLower = userInput.toLowerCase();
+// ***************Q2**********************
+// var x = "MuHammad Shariq";
+// x = x.toLowerCase();
+// ***************Q3**********************
+// var y = "Uniliver"
+// y = y.toUpperCase();
+// ***************Q4**********************
+// var x = "MuHammad Shariq";
+// var y = x.toLowerCase();
+// ***************Q5**********************
+// var arr =["Karachi","lahore"]
+// var arrLower = arr.tolowerCase();
+// ***************Q6**********************
+// var y = "Uniliver"
+// y = y.toUpperCase();
+// alert(y);
+// ***************Q7**********************
+// var cityName = "kaRacHi";
+// var firstAlpha = cityName.slice(0,1).toUpperCase();
+// var last = cityName.slice(1).toLocaleLowerCase();
+// var combo = firstAlpha + last ;
+// console.log(combo);
+// ***************chapter 22 to 25 **********************
+// var sameWords = "Captain";
+// var actual = sameWords.indexOf("ap");
+// if (actual !== -1){
+//     sameWords = sameWords.slice(0,actual) + "" + sameWords.slice(actual+2);
+// }
+// console.log(sameWords);
+// ***************Q3 **********************
+// var animal = "elephant";
+// var seg = animal.slice(2,6);
+// console.log(seg);
+// ***************Q4 **********************
+// var animal = "elephant";
+// var seg = animal.length;
+// console.log(seg);
+// ***************Q5 **********************
+// var chara = "I am an Engineer";
+// var charaNum = chara.length;
+// var charaLast = chara.slice(0,1) + chara.slice(13,16);
+// console.log(charaNum);
+// console.log(charaLast);
+// ***************Q6 **********************
+// var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// console.log("The value is "+ indx);
+// ***************Q7 **********************
+// var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// console.log("The value is "+ indx);
+// ***************Q8 **********************
+// var text = "go";
+// var indx = text.indexOf("g");
+// console.log(indx);
+// ***************Q9 **********************
+// var text = "go";
+// var indexNum = text.indexOf("g");
+// if (text.charAt(indexNum) !== undefined){
+//     console.log("match");
+// }
+// ***************Q10 **********************
+// var text = "abcde";
+// var indexChk = text.charAt(2);
+// console.log(indexChk);
+// ***************Q11 **********************
+// var text = "abcdefghijk"
+// var cha = text.charAt(10);
+// console.log(cha);
+// ***************Q12 **********************
+// var str = "abcdefghijk"
+// var x = str.charAt(str.length-1);
+// console.log(x);
+// ***************Q14 **********************
+// var str = "abcdefghijk"
+// if (str.charAt(5)=== "f"){
+// console.log("identified")
+// }
+// ***************Q15 **********************
+// var str = "WWW"
+// var strarr =["World","Wide","Web"]
+// for(var i=0 ; i<str.length;i++){
+//     if(i===0){
+//         console.log(str.charAt(i)+ "Stands For" + strarr[i])
+//     }
+//     else if(i===1){
+//         console.log(str.charAt(i)+ "Stands For" + strarr[i])
+//     }
+//     else if(i===2){
+//         console.log(str.charAt(i)+ "Stands For" + strarr[i])
+//     }
+// }
+// ***************Q16 **********************
